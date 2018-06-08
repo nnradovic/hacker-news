9602ab1f8e729660702979e950d5a0122a3031d5
nnradovic <nnradovic@gmail.com>
2018-06-08 18:49:37 +0200

    Inital commit setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./App.css";
+// import "./App.scss";
+
+class App extends Component {
+  render() {
+    return <h2>Test</h2>;
+  }
+}
+
+export default App;
