1e9244f116e9faa890e723d5302080161cfe35d2
root <nnradovic@gmail.com>
2018-06-09 18:45:39 +0200

    dispaly comments

--- src/StorySingle.js
+++ src/StorySingle.js
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { apiService } from "./apiservice/Apiservice";
 import Story from "./entites/Story";
+import Comments from "./entites/Comments";
 
 class StorySingle extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      story: null
+      story: null,
+      comments: null
     };
   }
   componentDidMount() {
-    return apiService
+    apiService
       .fetchSinlgeStory(this.props.match.params.id)
       .then(topStory => {
         return new Story(topStory);
@@ -19,27 +21,35 @@ class StorySingle extends Component {
         this.setState({
           story: topStory
         });
+      })
+      .then(topStory => {
+        if (this.state.story !== null) {
+          apiService
+            .fetchComments(this.state.story.kids[0])
+            .then(comments => {
+              return new Comments(comments);
+            })
+            .then(comments => {
+              this.setState({
+                comments: comments
+              });
+            });
+        }
       });
-
-    // return apiService
-    //   .fetchComments(this.props.match.params.id)
-    //   .then(topStory => {
-    //     return new Story(topStory);
-    //   })
-    //   .then(topStory => {
-    //     this.setState({
-    //       comments: topStory
-    //     });
-    //   });
   }
   render() {
-    if (this.state.story === null) {
-      return <p>Loading...</p>;
+    if (this.state.story === null && this.state.comments === null) {
+      return <p> Loading... </p>;
     }
 
-    console.log(this.state.story);
+    console.log(this.state.comments);
 
-    return <h1>{this.state.story.author}</h1>;
+    return (
+      <Fragment>
+        <h1> {this.state.story.author} </h1>
+        {/* <p>{this.state.comments.text}</p> */}
+      </Fragment>
+    );
   }
 }
 
