508cedefe211331aecc1f0aea17f3b85afbf10c1
root <nnradovic@gmail.com>
2018-06-10 15:30:12 +0200

    mainpage-singlepage logic finished

--- src/StorySingle.js
+++ src/StorySingle.js
@@ -2,13 +2,15 @@ import React, { Component, Fragment } from "react";
 import { apiService } from "./apiservice/Apiservice";
 import Story from "./entites/Story";
 import Comments from "./entites/Comments";
+import CommentsList from "./CommentsList";
 
 class StorySingle extends Component {
   constructor(props) {
     super(props);
     this.state = {
       story: null,
-      comments: null
+      comments: null,
+      isLoaded: false
     };
   }
   componentDidMount() {
@@ -23,31 +25,40 @@ class StorySingle extends Component {
         });
       })
       .then(topStory => {
-        if (this.state.story !== null) {
-          apiService
-            .fetchComments(this.state.story.kids[0])
-            .then(comments => {
-              return new Comments(comments);
-            })
-            .then(comments => {
-              this.setState({
-                comments: comments
-              });
-            });
-        }
+        console.log(this.state.story.kids);
+        return this.state.story.kids.map((kid, i) => {
+          return apiService.fetchComments(kid);
+        });
+      })
+      .then(x => {
+        console.log(x);
+        return Promise.all(x);
+      })
+      .then(y => {
+        console.log(y);
+        this.setState({
+          comments: y,
+          isLoaded: true
+        });
       });
   }
   render() {
     if (this.state.story === null && this.state.comments === null) {
       return <p> Loading... </p>;
     }
-
-    console.log(this.state.comments);
-
+    if (this.state.isLoaded) {
+      console.log(this.state.comments[0].text);
+    }
     return (
       <Fragment>
         <h1> {this.state.story.author} </h1>
-        {/* <p>{this.state.comments.text}</p> */}
+        {this.state.isLoaded ? (
+          this.state.comments.map((comment, i) => {
+            return <CommentsList key={i} comment={comment} />;
+          })
+        ) : (
+          <p> Loading... </p>
+        )}
       </Fragment>
     );
   }
